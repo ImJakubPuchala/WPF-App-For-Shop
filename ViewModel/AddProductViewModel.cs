a163350c479f2546f9f5d4b38855dfbeb670c155
ImJakubPuchala <86433812+ImJakubPuchala@users.noreply.github.com>
2024-01-30 22:14:30 +0100

    Ability to scan a product to avoid entering the EAN code, when adding a product and when editing the price

--- ViewModel/AddProductViewModel.cs
+++ ViewModel/AddProductViewModel.cs
@@ -8,13 +8,22 @@ namespace WPF_App_For_Shop.ViewModel;
 
 public class AddProductViewModel : INotifyPropertyChanged
 {
+    private ApiService _apiService;
     private string _name;
     private double _price;
     private string _ean;
-    private ApiService _apiService;
     public ICommand AddProductCommand { get; }
     public event PropertyChangedEventHandler PropertyChanged;
 
+    public string EAN
+    {
+        get => _ean;
+        set
+        {
+            _ean = value;
+            OnPropertyChanged(nameof(EAN));
+        }
+    }
     public string Name
     {
         get => _name;
@@ -35,33 +44,31 @@ public class AddProductViewModel : INotifyPropertyChanged
         }
     }
 
-    public string EAN
-    {
-        get => _ean;
-        set
-        {
-            _ean = value;
-            OnPropertyChanged(nameof(EAN));
-        }
-    }
+    
 
     public AddProductViewModel()
     {
         _apiService = new ApiService();
         AddProductCommand = new RelayCommand(AddProduct);
+
+        SerialPortModel.Instance.DataReceived += OnDataReceived;
+    }
+
+    private void OnDataReceived(string data)
+    {
+        EAN = data;
     }
 
     private async void AddProduct()
-    {
-        var product = new ProductInformation
-        {
-            Name = this.Name,
-            Price = this.Price,
-            EAN = this.EAN
-        };
-
+    { 
         try
         {
+            var product = new ProductInformation
+            {
+                Name = this.Name,
+                Price = this.Price,
+                EAN = this.EAN
+            };
             await _apiService.AddProductAsync(product);
         }
         catch (Exception ex)
@@ -73,5 +80,10 @@ public class AddProductViewModel : INotifyPropertyChanged
     protected virtual void OnPropertyChanged(string propertyName)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    ~AddProductViewModel()
+    {
+        SerialPortModel.Instance.DataReceived -= OnDataReceived;
     }
 }
--- ViewModel/ChangePriceViewModel.cs
+++ ViewModel/ChangePriceViewModel.cs
@@ -1,33 +1,76 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Input;
 using WPF_App_For_Shop.Commands;
 using WPF_App_For_Shop.Model;
 
 namespace WPF_App_For_Shop.ViewModel;
 
-public class ChangePriceViewModel
+public class ChangePriceViewModel : INotifyPropertyChanged
 {
     private readonly ApiService _apiService;
+    private string _ean;
+    private double _newPrice;
     public ICommand ChangePriceCommand { get; }
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public string EAN
+    {
+        get => _ean;
+        set
+        {
+            _ean = value;
+            OnPropertyChanged(nameof(EAN));
+        }
+    }
+
+    public double NewPrice
+    {
+        get => _newPrice;
+        set
+        {
+            _newPrice = value;
+            OnPropertyChanged(nameof(NewPrice));
+        }
+    }
 
     public ChangePriceViewModel()
     {
         _apiService = new ApiService();
         ChangePriceCommand = new RelayCommand(ChangePrice);
+
+        SerialPortModel.Instance.DataReceived += OnDataReceived;
     }
 
-    public ProductNewPrice ProductNewPrice { get; set; } = new ProductNewPrice();
-
+    private void OnDataReceived(string data)
+    {
+        EAN = data;
+    }
 
     private async void ChangePrice()
     {
         try
         {
-            await _apiService.ChangeProductPriceAsync(ProductNewPrice);
+            var productNewPrice = new ProductNewPrice
+            {
+                EAN = this.EAN,
+                NewPrice = this.NewPrice
+            };
+            await _apiService.ChangeProductPriceAsync(productNewPrice);
         }
         catch (Exception ex)
         {
             MessageBox.Show(ex.Message);
         }
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    ~ChangePriceViewModel()
+    {
+        SerialPortModel.Instance.DataReceived -= OnDataReceived;
     }
 }
